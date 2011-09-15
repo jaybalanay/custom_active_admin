124c1543dceb2e41c8a8a599a9ac77ea01b126fe
Manolet Balanay <manolet@proudcloud.net>
2011-09-15 14:11:58 +0800

    Printing Marker on View

--- app/views/active_admin/devise/registration/new.html.rb
+++ app/views/active_admin/devise/registration/new.html.rb
@@ -2,6 +2,7 @@
   <h2><%= title "#{active_admin_application.site_title} Login" %></h2>
 
   <% scope = Devise::Mapping.find_scope!(resource_name) %>
+	<%= "#{scope} | #{resource_name}" %>
   <%= active_admin_form_for(resource, :as => resource_name, :url => send(:"#{scope}_session_path"), :html => { :id => "session_new" }) do |f| 
     f.inputs do
       f.input :email
